f40810215b33dd2bffe755dbde7acd0759cca6ae
victor <zhanghaifeng903@sina.com>
2017-11-21 16:37:38 +0800

    modify build config

--- app/src/main/java/com/victor/util/Constant.java
+++ app/src/main/java/com/victor/util/Constant.java
@@ -8,7 +8,10 @@ import java.net.URLEncoder;
  * Created by victor on 2015/12/25.
  */
 public class Constant {
-    public static final boolean isDebug = true;
+    public static final boolean MODEL_DEBUG = true;
+    public static final boolean MODEL_ONLINE = true;
+    public static final int BUILD_CODE = 0;
+
     public static final String MA_DATA                              = "madata";
     public static final String SHARE_TYPE                           = "text/plain";
     public static final String ACTION_KEY                           = "ACTION_KEY";
